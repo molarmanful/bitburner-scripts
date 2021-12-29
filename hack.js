a76e7cce0f78de2082af9ecf6d5ddee2d3a8523e
Benjamin Pang <molarmanful@users.noreply.github.com>
2021-12-30 02:52:26 +0400

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,20 +1,45 @@
 /** @param {NS} ns **/
+
+// run hack.js [target]
+
+import {getNuked} from 'nuke-helpers.js'
+
 export async function main(ns) {
     let [target] = ns.args
-    let monLim = ns.getServerMaxMoney(target) * .69
-    let secLim = ns.getServerMinSecurityLevel(target) + 5
+
+    ns.disableLog('ALL')
+    ns.enableLog('print')
+    ns.enableLog('sleep')
+
+    for(let host of getNuked(ns)){
+        await ns.scp('serf.js', host)
+    }
     
-    if(ns.fileExists('BruteSSH.exe', 'home')) ns.brutessh(target)
-    ns.nuke(target)
     while(1){
-        if(ns.getServerSecurityLevel(target) > monLim){
-            await ns.weaken(target)
+        let secLim = ns.getServerMinSecurityLevel(target) + 3
+        let monLim = ns.getServerMaxMoney(target) * .9
+        let hackCount = .05
+
+        hackCount += 2
+
+        let mode
+
+        if(ns.getServerSecurityLevel(target) > secLim) mode = 0 // weaken
+        else if(ns.getServerMoneyAvailable(target) < monLim) mode = 1 // grow
+        else mode = 2 // hack
+
+        ns.print('mode: ' + mode)
+        ns.print(`sec: ${ns.getServerSecurityLevel(target)}/${secLim}`)
+        ns.print(`mon: ${ns.getServerMoneyAvailable(target)}/${monLim}`)
+
+        for(let host of getNuked(ns)){
+            ns.killall(host)
+            let thrCount = 0 | ns.getServerMaxRam(host) / ns.getScriptRam('serf.js')
+            if(thrCount > 0){
+                ns.exec('serf.js', host, thrCount, mode, target);
+            }
         }
-        else if(ns.getServerMoneyAvailable(target) < secLim){
-            await ns.grow(target)
-        }
-        else {
-            await ns.hack(target)
-        }
+
+        await ns.sleep(hackCount * [ns.getWeakenTime, ns.getGrowTime, ns.getHackTime][mode](target) + 300)
     }
 }
