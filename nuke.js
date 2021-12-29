6dad4993a7cb41e0d1e4ce5b9d0372b8699b015e
Benjamin Pang <molarmanful@users.noreply.github.com>
2021-12-30 02:51:18 +0400

    Create nuke.js

---
+++ nuke.js
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+
+import {getServers} from 'nuke-helpers.js'
+
+export async function main(ns) {
+    ns.print(getServers(ns))
+    for(let host of getServers(ns)){
+        if(!ns.hasRootAccess(host) && ns.getHackingLevel() >= ns.getServerRequiredHackingLevel(host)){
+            let ports = ns.getServerNumPortsRequired(host)
+            try {
+                if(ports > 0) ns.brutessh(host)
+                if(ports > 1) ns.ftpcrack(host)
+                if(ports > 2) ns.relaysmtp(host)
+                if(ports > 3) ns.httpworm(host)
+                if(ports > 4) ns.sqlinject(host)
+                ns.nuke(host)
+            }
+            catch(e){}
+        }
+    }
+}
