f32cea7514364811f07cb551cd46372dca58af2f
Benjamin Pang <molarmanful@users.noreply.github.com>
2021-12-30 03:02:12 +0400

    Create hacknet.js

---
+++ hacknet.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    let hn = ns.hacknet
+    let canBuy = x=> x < ns.getServerMoneyAvailable('home') / 2
+    while(1){
+        if(canBuy(hn.getPurchaseNodeCost())){
+            hn.purchaseNode()
+        }
+        for(let i in [...new Array(hn.numNodes())]){
+            let d = hn.getNodeStats(i).level % 10
+            if(canBuy(hn.getLevelUpgradeCost(i, 10 - d))){
+                hn.upgradeLevel(i, 10 - d)
+            }
+            if(canBuy(hn.getRamUpgradeCost(i, 10 - d))){
+                hn.upgradeRam(i, 10 - d)
+            }
+            if(canBuy(hn.getCoreUpgradeCost(i, 10 - d))){
+                hn.upgradeCore(i, 10 - d)
+            }
+        }
+        await ns.sleep(1000)
+    }
+}
