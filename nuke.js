b303c89f12bde0982052a6610640a398cf26ee3c
Benjamin Pang <molarmanful@users.noreply.github.com>
2021-12-30 04:59:03 +0400

    Update nuke.js

--- nuke.js
+++ nuke.js
@@ -5,19 +5,27 @@
 import {getServers} from 'nuke-helpers.js'
 
 export async function main(ns) {
-    ns.print(getServers(ns))
-    for(let host of getServers(ns)){
-        if(!ns.hasRootAccess(host) && ns.getHackingLevel() >= ns.getServerRequiredHackingLevel(host)){
-            let ports = ns.getServerNumPortsRequired(host)
-            try {
-                if(ports > 0) ns.brutessh(host)
-                if(ports > 1) ns.ftpcrack(host)
-                if(ports > 2) ns.relaysmtp(host)
-                if(ports > 3) ns.httpworm(host)
-                if(ports > 4) ns.sqlinject(host)
-                ns.nuke(host)
+    ns.disableLog('ALL')
+    ns.enableLog('print')
+
+    while(1){
+        for(let host of getServers(ns)){
+            if(!ns.hasRootAccess(host) && ns.getHackingLevel() >= ns.getServerRequiredHackingLevel(host)){
+                let ports = ns.getServerNumPortsRequired(host)
+                ns.print(`host: ${host}, lvl${ns.getServerRequiredHackingLevel(host)}, ports: ${ports}`)
+                try {
+                    if(ports > 0) ns.brutessh(host)
+                    if(ports > 1) ns.ftpcrack(host)
+                    if(ports > 2) ns.relaysmtp(host)
+                    if(ports > 3) ns.httpworm(host)
+                    if(ports > 4) ns.sqlinject(host)
+                    ns.nuke(host)
+                }
+                catch(e){
+                    ns.print(e)
+                }
             }
-            catch(e){}
         }
+        await ns.sleep(1000)
     }
 }
